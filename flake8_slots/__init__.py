9031d59653773365f4dc6f2ecbd7ad75e258ffe6
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-18 10:30:26 +0000

    Bump version v0.1.3 -> v0.1.4

--- flake8_slots/__init__.py
+++ flake8_slots/__init__.py
@@ -37,7 +37,7 @@ import flake8_helper
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.3"
+__version__: str = "0.1.4"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["Visitor", "Plugin", "SLOT000", "SLOT001", "SLOT002"]
